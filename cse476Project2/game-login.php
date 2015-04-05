0420cc32ae68f82c5653557966f50c515635ec53
SpartyWare <richa779@msu.edu>
2015-04-05 17:59:48 -0400

    Pushing web project

---
+++ cse476Project2/game-login.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SpartyWare
+ * Date: 4/4/15
+ * Time: 3:38 PM
+ */
+require_once "db.inc.php";
+echo '<?xml version="1.0" encoding="1.0" ?>';
+
+/*if(!isset($_GET['magic']) || $_GET['magic'] != "NechAtHa6RuzeR8x") {
+    echo '<login status="no" msg="magic" />';
+    exit;
+}*/
+
+if(!isset($_GET['username']) || !isset($_GET['password'])) {
+    echo '<birdGame status="no" msg="Username or Password Missing" />';
+    exit;
+}
+
+
+/*$user = $_GET['username'];
+$password = $_GET['password'];
+echo <<<XML
+<tag user="$user" pw="$password" />
+XML;
+*/
+
+$pdo = pdo_connect();
+
+$user = getUser($pdo, $_GET['username'], $_GET['password']);
+
+/**
+ * Ask the database for the user ID. If the user exists, the password
+ * must match.
+ * @param $pdo PHP Data Object
+ * @param $user The user name
+ * @param $password Password
+ * @return id if successful or exits if not
+ */
+function getUser($pdo, $user, $password) {
+    // Does the user exist in the database?
+    $userQ = $pdo->quote($user);
+    $query = "SELECT id,username,password from birdUser where username=$userQ";
+
+    $rows = $pdo->query($query);
+    if($row = $rows->fetch()) {
+        // We found the record in the database
+        // Check the password
+        if($row['password'] != $password) {
+            echo '<birdGame status="no" msg="password error" />';
+            exit;
+        }
+        echo '<birdGame status ="yes"/>';
+        return $row;
+    }
+    echo '<birdGame status="no" msg="user error" />';
+    exit;
+}
---
+++ cse476Project2/new-user.php
@@ -0,0 +1,91 @@
+<?php
+require_once "db.inc.php";
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+
+// Ensure the xml post item exists
+if(!isset($_POST['xml'])) {
+    echo '<birdGame status="no" msg="missing XML" />';
+    exit;
+}
+
+processXml(stripslashes($_POST['xml']));
+
+/**
+ * Process the XML query
+ * @param $xmltext the provided XML
+ */
+function processXml($xmltext) {
+    // Load the XML
+    $xml = new XMLReader();
+    if(!$xml->XML($xmltext)) {
+        echo '<birdGame status="no" msg="invalid XML" />';
+        exit;
+    }
+
+    // Connect to the database
+    $pdo = pdo_connect();
+
+    // Read to the start tag
+    while($xml->read()) {
+        if ($xml->nodeType == XMLReader::ELEMENT && $xml->name == "birdGame") {
+            // We have the bird tag
+            $magic = $xml->getAttribute("magic");
+            if ($magic != "NechAtHa6RuzeR8x") {
+                echo '<birdGame status="no" msg="magic" />';
+                exit;
+            }
+
+            while ($xml->read()){
+                if($xml->nodeType == XMLReader::ELEMENT && $xml->name == "newuser"){
+                    $username = $xml->getAttribute("username");
+                    $password = $xml->getAttribute("password");
+                    if(newUser($pdo, $username, $password)){
+                        echo '<birdGame status="yes"/>';
+                        exit;
+                    }
+                    echo '<birdGame status="no" msg="Username Already Exists">';
+                }
+            }
+        }
+    }
+    echo '<birdGame save="no" msg="invalid XML" />';
+
+}
+
+/**
+ * Ask the database for the user ID. If the user exists, the password
+ * must match.
+ * @param $pdo PHP Data Object
+ * @param $user The user name
+ * @param $password Password
+ * @return id if successful or exits if not
+ */
+function newUser($pdo, $username, $password) {
+
+    if(userExists($pdo,$username)) {
+
+        // Does the user exist in the database?
+        $query = <<<QUERY
+REPLACE INTO birdUser(username, password)
+VALUES($username, $password)
+QUERY;
+        if (!$pdo->query($query)) {
+            echo '<birdGame status="no" msg="insertfail">' . $query . '</birdGame>';
+            exit;
+        }
+        return true;
+    }
+    return false;
+}
+
+function userExists($pdo, $username){
+    // Does the user exist in the database?
+    $userQ = $pdo->quote($username);
+    $query = "SELECT id,username,password from birdUser where username=$userQ";
+
+    $rows = $pdo->query($query);
+    if($rows != 0) {
+        return false;
+    }
+    return true;
+}
